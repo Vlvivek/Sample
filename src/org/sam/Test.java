12059e95acbbd32fe0532ba1dc15e865d152ab9d
vith2294 <vivekmech012@gmail.com>
2023-09-16 12:08:52 +0530

    push to git

---
+++ src/org/sam/Test.java
@@ -0,0 +1,16 @@
+package org.sam;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		String s = "Java";
+		String s1 = "Java";
+		boolean equals = s.equals(s);
+		
+		boolean equals2 = s1.equals(s1);
+		
+		System.out.println(equals);
+	}
+
+}
