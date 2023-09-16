b3328562d00291e54db256977969b0bb179736f9
vith2294 <vivekmech012@gmail.com>
2023-09-16 13:13:35 +0530

    B's Rework done

--- src/org/sam/Test.java
+++ src/org/sam/Test.java
@@ -1,16 +1,23 @@
 package org.sam;
 
 public class Test {
-	
+
 	public static void main(String[] args) {
-		
+
 		String s = "Java";
-		String s1 = "Java";
+		String s1 = "JaVa";
 		boolean equals = s.equals(s);
-		
-		boolean equals2 = s1.equals(s1);
-		
+
+		boolean equals2 = s1.equals(s);
+
 		System.out.println(equals);
+		System.out.println(equals2);
+
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.println("Cube Of the Number:" + (i * i * i));
+
+		}
 	}
 
 }
